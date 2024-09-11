b1767f187d5ec3c8e3c7d7dbe5b43202e006eadc
yaeun <nyeshinee0525@naver.com>
2024-09-11 17:21:40 +0900

    Login View

---
+++ _modooLCK_iOS_/presentation/Login/LoginUI.swift
@@ -0,0 +1,43 @@
+//
+//  LoginUI.swift
+//  modoo_LCK_iOS
+//
+//  Created by 나예은 on 2024/09/09.
+//
+
+import SwiftUI
+
+struct LoginUI: View {
+    var body: some View {
+            ZStack {
+              
+                Image("bg")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack{
+                    
+                    Image("full_logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 274)
+                        .offset(y:10)
+                        .padding(.bottom,120)
+                    
+                   Button (action: {
+                        print("kakaoBtn")
+                    }){
+                        Image("kakao")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 183)
+                    }
+                }
+            }
+    }
+}
+
+#Preview {
+    LoginUI()
+}
